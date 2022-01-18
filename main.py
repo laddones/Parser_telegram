f68bddefd83eac115fc7dbba2a15988d4064df24
Laddones <savsadasavsada@gmail.com>
2022-01-18 18:39:10 +0200

    Modificate

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import openpyxl
+import re
 
 
 def get_html(url):
@@ -12,41 +13,42 @@ def get_html(url):
     try:
         driver.get(url=url)
         input()
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        while True:
-            if soup.find('div', {'id': 'message1'}):
-                break
+        # soup = BeautifulSoup(driver.page_source, 'html.parser')
+        for i in range(1, 10):
 
             find_more_element = driver.find_element(By.XPATH,
-                                                    '/html/body/div[1]/div/div/div[2]/div[3]/div[2]/div[2]/div[1]/div/div[1]')
+                                                    '/html/body/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/div/div/div[2]')
             actions = ActionChains(driver)
             actions.move_to_element(find_more_element).perform()
             time.sleep(0.3)
 
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             try:
-                all_message = soup.find_all('div', {"class": 'Message'})
+                all_message = soup.find_all('div', {"class": 'bubble'})
                 for ms in all_message:
                     try:
                         message.add(ms)
                     except:
                         print('Error')
                 print(len(message))
-                if len(message) > 500:
-                    add_to_exel(message)
-                    message = set()
-
-                    print('Продолжить - ? Y/N')
-                    process = str(input().lower())
-                    if process == 'n':
-                        break
-
             except:
                 print('Error')
-
-        # add_to_exel(message)
-
-        print('*'*4)
+        add_to_exel(message)
+        #         if len(message) > 500:
+        #             add_to_exel(message)
+        #             message = set()
+        #
+        #             print('Продолжить - ? Y/N')
+        #             process = str(input().lower())
+        #             if process == 'n':
+        #                 break
+        #
+        #     except:
+        #         print('Error')
+        #
+        # # add_to_exel(message)
+        #
+        # print('*'*4)
         input()
 
     except Exception as ex:
@@ -61,32 +63,72 @@ def add_to_exel(message_obj):
     ws = excel_obj.active
     for ms in message_obj:
         try:
-            message_id = ms.find('div', {"class": "bottom-marker"}).get('data-message-id')
+            user_id = ms.find('div', {"class": "name"}).get('data-peer-id')
+        except:
+            user_id = 'Not_found'
+        try:
+            message_data = ms.find('span', {"class": 'time'}).get('title')
+            message_data_text = str(message_data)
+            if message_data.find('Edited') != -1:
+                message_data_edited_index = message_data.find('Edited')
+                message_data_text = message_data[:message_data_edited_index]
+                if message_data.find('Original') != -1:
+                    message_data_original = message_data[message_data.find('Original'):]
+                    message_data_edited = message_data[message_data_edited_index:message_data.find('Original')]
+                else:
+                    message_data_edited = message_data[message_data_edited_index:]
+                    message_data_original = 'Not_found'
+            elif message_data.find('Original') != -1:
+                message_data_original = message_data[message_data.find('Original'):]
+                message_data_text = message_data[:message_data.find('Original')]
+                message_data_edited = 'Not_found'
+            else:
+                message_data_edited = 'Not_found'
+                message_data_original = 'Not_found'
+        except:
+            message_data = 'Not_found'
+            message_data_text = 'Not_found'
+            message_data_edited = 'Not_found'
+            message_data_original = 'Not_found'
+        try:
+            name = ms.find('div', {"class": "name"}).get_text()
+        except:
+            name = 'Not_found'
+        try:
+            article = ms.find('div', {"class": 'message'}).get_text()
+        except:
+            article = 'Not_found'
+        try:
+            reply_name_id = ms.find('div', {"class": 'reply-title'}).find('span', {"class": "peer-title"}).get('data-peer-id')
+        except:
+            reply_name_id = 'Not_found'
+        try:
+            reply_name = ms.find('div', {"class": 'reply-title'}).find('span', {"class": "peer-title"}).get_text()
+        except:
+            reply_name = 'Not_found'
+        try:
+            reply_article = ms.find('div', {"class": "reply-subtitle"}).get_text()
+        except:
+            reply_article = 'Not_found'
 
-        except:
-            message_id = 'Not_found'
-        try:
-            message_text = ms.find('p', {"class": 'text-content'}).get_text()
-        except:
-            message_text = 'Not_found'
-        try:
-            message_author = ms.find('div', {"class": "message-title"}).get_text()
-        except:
-            message_author = 'Not_found'
-        try:
-            message_time = ms.find('span', {"class": "message-time"}).get_text()
-        except:
-            message_time = 'Not_found'
-
-        ws[f'A{str(ws.max_row+1)}'] = int(message_id)
-        ws[f'B{str(ws.max_row)}'] = message_text
-        ws[f'C{str(ws.max_row)}'] = message_author
-        ws[f'D{str(ws.max_row)}'] = message_time
+        ws[f'A{str(ws.max_row+1)}'] = user_id
+        ws[f'B{str(ws.max_row)}'] = name
+        ws[f'C{str(ws.max_row)}'] = article
+        ws[f'D{str(ws.max_row)}'] = reply_name_id
+        ws[f'E{str(ws.max_row)}'] = reply_name
+        ws[f'F{str(ws.max_row)}'] = reply_article
+        ws[f'G{str(ws.max_row)}'] = message_data_text
+        ws[f'H{str(ws.max_row)}'] = re.sub(r'\w+\: ', '', message_data_edited)
+        ws[f'I{str(ws.max_row)}'] = re.sub(r'\w+\: ', '', message_data_original)
+        print()
+        print(message_data)
+        # ws[f'C{str(ws.max_row)}'] = message_author
+        # ws[f'D{str(ws.max_row)}'] = message_time
 
     excel_obj.save('Sheat.xlsx')
 
 
 if __name__ == '__main__':
-    get_html('https://web.telegram.org/z/#-1330723289')
+    get_html('https://xn--80affa3aj0al.xn--80asehdb/')
 
     # parsing_telegram()
